aec84e9288d1d369ffcb60f3f8cb64cbc78e1ea3
Aryan <aryanyadav@cyntexa.com>
2025-06-02 16:15:55 +0530

    app

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ const activity = require('./routes/activity');
 
 const app = express();
 
-app.use(helmet()); 
+// Use helmet for standard security headers
+app.use(helmet());
+
+// Explicitly add nosniff to all responses (in case helmet skips for static HTML)
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
+
 app.disable('x-powered-by');
 
-app.use(express.static(path.join(__dirname, '/public'), {
-  setHeaders: (res, path) => {
-    res.setHeader('X-Content-Type-Options', 'nosniff'); 
-  }
-}));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// API endpoints
 app.post('/client-credentials/', activity.fetchClientCredentials);
 app.post('/fetch-external-key/', activity.fetchExternalKey);
 app.post('/save/', activity.save);
@@ -25,6 +31,7 @@ app.post('/validate/', activity.validate);
 app.post('/publish/', activity.publish);
 app.post('/execute/', activity.execute);
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
