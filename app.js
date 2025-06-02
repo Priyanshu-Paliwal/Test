49b93fae52ec68d79b1f81042abde5a676475645
Aryan <aryanyadav@cyntexa.com>
2025-06-02 17:38:02 +0530

    app

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const helmet = require('helmet');
-const activity = require('./routes/activity');
-
+var activity = require('./routes/activity');
 const app = express();
 
-// 1. Disable 'x-powered-by'
-app.disable('x-powered-by');
-
-// 2. Serve static files with `X-Content-Type-Options: nosniff`
-app.use(express.static(path.join(__dirname, '/public'), {
-  setHeaders: (res, filePath) => {
-    res.setHeader('X-Content-Type-Options', 'nosniff');
-  }
-}));
-
-// 3. Apply helmet with HSTS settings
-app.use(helmet({
-  hsts: {
-    maxAge: 63072000, // 2 years
-    includeSubDomains: true,
-    preload: true
-  }
-}));
-
-// 4. Body parsers
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// 5. Routes
 app.post('/client-credentials/', activity.fetchClientCredentials);
 app.post('/fetch-external-key/', activity.fetchExternalKey);
 app.post('/save/', activity.save);
@@ -37,6 +15,6 @@ app.post('/validate/', activity.validate);
 app.post('/publish/', activity.publish);
 app.post('/execute/', activity.execute);
 
-// 6. Start server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT);
