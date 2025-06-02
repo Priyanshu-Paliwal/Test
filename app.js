093a23840df1d5671b56fffb96e114e33e76da92
Aryan <aryanyadav@cyntexa.com>
2025-06-02 14:10:09 +0530

    app

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const express = require('express');
 const path = require('path');
 var activity = require('./routes/activity');
 const app = express();
-const helmet = require('helmet');
 
+app.disable('x-powered-by');
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(helmet({
-  contentSecurityPolicy: false
-}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
