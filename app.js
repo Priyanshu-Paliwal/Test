c077951f082f39dc8c066bbad044fb9b315703f4
Aryan <aryanyadav@cyntexa.com>
2025-06-02 13:48:43 +0530

    added helmet

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const path = require('path');
 var activity = require('./routes/activity');
 const app = express();
+const helmet = require('helmet');
 
+app.use(helmet());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
