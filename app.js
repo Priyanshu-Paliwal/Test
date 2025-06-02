19e55b36c3fdcd11b15466769e3c5b25da9bdf97
Aryan <aryanyadav@cyntexa.com>
2025-06-02 17:15:32 +0530

    app

--- app.js
+++ app.js
@@ -39,6 +39,4 @@ app.post('/execute/', activity.execute);
 
 // 6. Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT);
