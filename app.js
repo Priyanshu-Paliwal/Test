733eeee3e67dc21af828c620b53ac6631235a5e0
Aryan <aryanyadav@cyntexa.com>
2025-06-02 13:51:34 +0530

    app

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var activity = require('./routes/activity');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(helmet());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
