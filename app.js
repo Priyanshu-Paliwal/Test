2ddb833f70ccd9adbcbc48ca163668010def1b30
Aryan <aryanyadav@cyntexa.com>
2025-06-02 16:11:40 +0530

    app

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-var activity = require('./routes/activity');
-const app = express();
 const helmet = require('helmet');
+const activity = require('./routes/activity');
 
+const app = express();
+
+app.use(helmet()); 
 app.disable('x-powered-by');
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public'), {
   setHeaders: (res, path) => {
-    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Content-Type-Options', 'nosniff'); 
   }
 }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.post('/client-credentials/', activity.fetchClientCredentials);
 app.post('/fetch-external-key/', activity.fetchExternalKey);
@@ -23,5 +26,6 @@ app.post('/publish/', activity.publish);
 app.post('/execute/', activity.execute);
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
